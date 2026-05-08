d503b15aacea21edb642d05e4ad6a31cd30a58e5
tommy <zombietommie@gmail.com>
2026-05-08 14:52:09 -0500

    adding more details to the comments of action points py file.

--- src/action_points.py
+++ src/action_points.py
@@ -1,4 +1,8 @@
-## This is the action point where we will set the limit for the players but the
-## Game master will have unlimited, but let's set a standard 25.
+# This is the action point where we will set the limit for the players but the
+# Game master will have unlimited, but let's set a standard 25.
+# will need to implement DB to persist the pool
 
-## will need to implement DB to persist the pool
+# According to the book players share the one Action Point Pool
+# The pool is maxed out at 5, any gained will be ignored.
+
+# First start with adding and then removing AP from the player pool
