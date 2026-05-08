689732d7e4a8e20bd1880723f369f5f846abe5fe
tommy <zombietommie@gmail.com>
2026-05-08 10:12:29 -0500

    modified the rebuild in update bot shell script

--- update_bot.sh
+++ update_bot.sh
@@ -4,9 +4,13 @@
 echo "Pulling latest code from GitHub..."
 git pull origin master
 
-echo "Rebuilding and restarting the Docker containers..."
-# --build ensures any new dependencies in requirements.txt or changes to the Dockerfile are applied
-# -d runs it in the background
-docker-compose up -d --build
+echo "--- Showing latest commit on server ---"
+git log -1 --pretty=format:"%h - %an, %ar : %s"
+echo # for a new line
 
-echo "Bot updated successfully!"
+echo "--- Rebuilding and restarting the bot container ---"
+# --force-recreate ensures the bot container is stopped and recreated
+# --build rebuilds the image if the code has changed
+docker-compose up -d --build --force-recreate bot
+
+echo "--- Bot update process finished! ---"
